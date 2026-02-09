318236f8f2d3721d83f8523acd73b0125acc0173
Utkarsh736 <utkarshtomar736@gmail.com>
2026-02-09 18:23:44 +0530

    [Update]: Read .txt in 8-byte chunks

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
-}
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
 
+	buffer := make([]byte, 8)
+
+	for {
+		n, err := file.Read(buffer)
+		
+		if n > 0 {
+			fmt.Printf("read: %s\n", buffer[:n])
+		}
+		
+		if err == io.EOF {
+			break
+		}
+		
+		if err != nil {
+			fmt.Println("Error reading file:", err)
+			return
+		}
+	}
+}
